f477774a12e482fa1d529fe3bd925ad960a69b6d
qolbudr <qolbu.dzikru30@gmail.com>
2021-04-22 13:40:02 +0700

    Complete product page

--- lib/page/product.dart
+++ lib/page/product.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:page_transition/page_transition.dart';
+import 'dart:convert';
 import '../elements/custom.dart' as custom;
+import '../elements/config.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
 
 class Product extends StatefulWidget {
   @override
@@ -12,6 +15,9 @@ class Product extends StatefulWidget {
 
 class _ProductState extends State<Product> {
   String _scanBarcode;
+  bool isLoading = true;
+  bool haveItem = false;
+  final searchController = TextEditingController();
 
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
@@ -28,6 +34,43 @@ class _ProductState extends State<Product> {
       _scanBarcode = barcodeScanRes;
     });
   }
+
+  void getProducts() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('loginToken');
+    String key = "Bearer $token";
+    Map<String, dynamic> json;
+
+    final response = await http.get(
+      Uri.parse("$url/api/auth/products"),
+      headers: <String, String> {
+        'Accept': 'application/json',
+        'Authorization': key,
+      },
+    );
+
+    if(this.mounted) {
+      try {
+        setState(() {
+          json = jsonDecode(response.body);
+          if(json.containsKey('name')) {
+            haveItem = true;
+            isLoading = false;
+          } else {
+            isLoading = false;
+          }
+        });
+      } on PlatformException {
+        print("Something was wrong");
+      }
+    }
+  }
+
+  void initState() {
+    super.initState();
+    getProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,19 +82,97 @@ class _ProductState extends State<Product> {
         ),
         title: Text("Products", style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold))
       ),
-      body: Container(
+      body: haveItem ?
+      Container(
         color: Colors.white,
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("images/box.png", width: 100),
-              SizedBox(height: 20),
-              Text("No product yet.", style: TextStyle(color: Colors.black, fontSize: 15))
-            ],
-          )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 78,
+                    child: custom.CustomTextField(controller: searchController, hintText: "Search", obscureText: false)
+                  ),
+                  IconButton(icon: Icon(Icons.search), onPressed: scanBarcodeNormal, color: Colors.blue)
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(left: 10, right:10, bottom: 10),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: scanBarcodeNormal,
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        elevation: MaterialStateProperty.all<double>(0),
+                        overlayColor: MaterialStateProperty.all<Color>(Colors.grey[200]),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black12)
+                          )
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 10),
+                              child: Image.asset('images/box.png', width: 40),
+                            ),
+                            Column(
+                              children: [
+                                Text("ajkshkjas", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black87)),
+                                Text("ajkshkjas", style: TextStyle(fontSize: 15, color: Colors.black87)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ],
+        ),
+      ) : 
+      Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [ 
+            isLoading ?
+            LinearProgressIndicator(minHeight: 1, backgroundColor: Colors.blue)
+            : SizedBox(height: 0),
+            SizedBox(height: MediaQuery.of(context).size.height*0.2),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("images/box.png", width: 100),
+                  SizedBox(height: 20),
+                  Text("No product yet.", style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text("Scan product's barcode to add your product to inventory", style: TextStyle(color: Colors.black, fontSize: 15), textAlign: TextAlign.center)
+                  ),
+                ],
+              ),
+            )
+          ]
         ),
       ),
       floatingActionButton: FloatingActionButton(
